f9b74b9553d96ce1d935ecba6aa9a37d2a98741f
Vladimir Bredikhin <brutal.v4@gmail.com>
2020-01-29 18:57:19 +0200

    Daily backup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Quiz from './containers/Quiz/Quiz';
+import Layout from './hoc/Layout/Layout';
 
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <Layout>
+      <Quiz />
+    </Layout>
   );
 }
 
