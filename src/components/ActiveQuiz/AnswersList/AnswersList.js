149e8528b85a43c9e215c2f9af7c3f57f96816cc
Vladimir Bredikhin <brutal.v4@gmail.com>
2020-01-30 08:53:16 +0200

    Update

--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -5,8 +5,14 @@ import classes from './AnswersList.module.css';
 const AnswersList = props => {
   return (
     <ul className={classes.AnswersList}>
-      {props.answers.map((answer, index) => {
-        return <AnswerItem answer={answer} key={index} />;
+      {props.answers.map(answer => {
+        return (
+          <AnswerItem
+            answer={answer}
+            key={answer.id}
+            onAnswerClick={props.onAnswerClick}
+          />
+        );
       })}
     </ul>
   );
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,16 +4,38 @@ import classes from './Quiz.module.css';
 
 class Quiz extends Component {
   state = {
+    activeQuestion: 0,
     quiz: [
       {
+        id: 1,
+        question: 'Какого цвета небо?',
+        correctAnswer: 2,
         answers: [
-          { text: 'Вопрос 1' },
-          { text: 'Вопрос 2' },
-          { text: 'Вопрос 3' },
-          { text: 'Вопрос 4' },
+          { id: 1, text: 'Черный' },
+          { id: 2, text: 'Синий' },
+          { id: 3, text: 'Красный' },
+          { id: 4, text: 'Зеленый' },
+        ],
+      },
+      {
+        id: 2,
+        question: 'В каком году основали Санкт-Петербург?',
+        correctAnswer: 3,
+        answers: [
+          { id: 1, text: '1700' },
+          { id: 2, text: '1702' },
+          { id: 3, text: '1703' },
+          { id: 4, text: '1803' },
         ],
       },
     ],
+  };
+
+  onAnswerClickHandler = answerId => {
+    console.log(answerId);
+    this.setState(prev => ({
+      activeQuestion: prev.activeQuestion + 1,
+    }));
   };
 
   render() {
@@ -21,7 +43,13 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Ответьте на все вопросы</h1>
-          <ActiveQuiz answers={this.state.quiz[0].answers} />
+          <ActiveQuiz
+            question={this.state.quiz[this.state.activeQuestion].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            onAnswerClick={this.onAnswerClickHandler}
+            quizLenght={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
+          />
         </div>
       </div>
     );
