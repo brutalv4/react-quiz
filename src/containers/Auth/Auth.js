ccda210a797dccb8e0564618754726cad7b6aca6
Vladimir Bredikhin <brutal.v4@gmail.com>
2020-02-10 18:30:42 +0200

    Daily backup

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.css';
 
 class Auth extends Component {
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+        </div>
       </div>
     );
   }
