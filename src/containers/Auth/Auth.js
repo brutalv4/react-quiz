e20ea6d9b81577bc85a8beb6b3641134fc21558a
Vladimir Bredikhin <brutal.v4@gmail.com>
2020-02-13 07:33:11 +0200

    Update Auth validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,35 @@ import Input from '../../components/UI/Input/Input';
 import classes from './Auth.module.css';
 
 class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
+
   loginHandler = () => {
     console.log('logged in');
   };
@@ -17,6 +46,44 @@ class Auth extends Component {
     console.log('submit');
   };
 
+  validateCtrl(value, validation) {
+    if (!validation) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (validation.email) {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      isValid = re.test(String(value).toLowerCase()) && isValid;
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
+  onChangeHandler = (event, ctrlName) => {
+    console.log(ctrlName, event.target.value);
+
+    const formControls = { ...this.state.formControls };
+    const ctrl = { ...formControls[ctrlName] };
+
+    ctrl.value = event.target.value;
+    ctrl.touched = true;
+    ctrl.valid = this.validateCtrl(ctrl.value, ctrl.validation);
+
+    formControls[ctrlName] = ctrl;
+
+    this.setState({ formControls });
+  };
+
   render() {
     return (
       <div className={classes.Auth}>
@@ -24,8 +91,24 @@ class Auth extends Component {
           <h1>Авторизация</h1>
 
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <Input label="Email" />
-            <Input label="Пароль" errorMessage={'TEST'} />
+            {Object.keys(this.state.formControls).map((ctrlName, index) => {
+              const ctrl = this.state.formControls[ctrlName];
+              return (
+                <Input
+                  key={ctrlName + index}
+                  type={ctrl.type}
+                  value={ctrl.value}
+                  shouldValidate={!!ctrl.validation}
+                  valid={ctrl.valid}
+                  touched={ctrl.touched}
+                  label={ctrl.label}
+                  errorMessage={ctrl.errorMessage}
+                  onChange={event => {
+                    this.onChangeHandler(event, ctrlName);
+                  }}
+                />
+              );
+            })}
             <Button type="success" onClick={this.loginHandler}>
               Войти
             </Button>
