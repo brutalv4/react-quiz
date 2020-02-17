fbe2286da667d92f1a2417de8d115de731294335
Vladimir Bredikhin <brutal.v4@gmail.com>
2020-02-17 17:25:46 +0200

    Add loader

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import Loader from '../../components/UI/Loader/Loader';
 import classes from './QuizList.module.css';
 
 class QuizList extends Component {
   state = {
     quizes: [],
+    loading: true,
   };
 
   async componentDidMount() {
@@ -18,24 +20,29 @@ class QuizList extends Component {
         quizes.push({ id, name: `Тест №${index + 1}` });
       });
 
-      this.setState({ quizes });
+      this.setState({
+        quizes,
+        loading: false,
+      });
     } catch (reason) {
       console.error(reason);
     }
   }
+
+  renderQuizes = () => {
+    return this.state.quizes.map(quiz => (
+      <li key={quiz.id}>
+        <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
+      </li>
+    ));
+  };
 
   render() {
     return (
       <div className={classes.QuizList}>
         <div>
           <h1>Список тестов</h1>
-          <ul>
-            {this.state.quizes.map(quiz => (
-              <li key={quiz.id}>
-                <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
-              </li>
-            ))}
-          </ul>
+          {this.state.loading ? <Loader /> : <ul>{this.renderQuizes()}</ul>}
         </div>
       </div>
     );
